7c07bcb2b35f079d91b1cfc70fba046874fca86e
tomcat <tomcat.cheng@microlife.com.tw>
2018-03-29 14:34:59 +0800

    init project & code.

---
+++ app/src/main/java/com/demo/tomcat/alarmnotification2/AlarmReceiver.java
@@ -0,0 +1,36 @@
+package com.demo.tomcat.alarmnotification2;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+import static android.support.v4.content.WakefulBroadcastReceiver.startWakefulService;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        // throw new UnsupportedOperationException("Not yet implemented");
+
+        MainActivity inst = MainActivity.instance();
+        inst.setAlarmText("Alarm! Wake up! Wake up!");
+
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null)
+        {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
+        ComponentName comp = new ComponentName(context.getPackageName(), AlarmService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+    }
+}
---
+++ app/src/main/java/com/demo/tomcat/alarmnotification2/MainActivity.java
@@ -0,0 +1,106 @@
+package com.demo.tomcat.alarmnotification2;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
+
+// https://javapapers.com/android/android-alarm-clock-tutorial/
+
+
+public class MainActivity extends AppCompatActivity
+{
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    TimePicker      alarmTimerPicker;
+    ToggleButton    alarmToggle;
+    TextView        alarmTextView;
+
+    AlarmManager    alarmManager;
+    PendingIntent   pendingIntent;
+
+
+
+    @SuppressLint("StaticFieldLeak")
+    private static MainActivity inst;
+
+    public static MainActivity instance()
+    {
+        Log.w(TAG, "instance(), ");
+        return inst;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        Log.w(TAG, "onCreate(), ");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initControl();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        Log.w(TAG, "onStart(), ");
+        super.onStart();
+        inst = this;
+    }
+
+    public void onToggleClicked(View view)
+    {
+        Log.w(TAG, "onToggleClicked(), ");
+        if (((ToggleButton)view).isChecked())
+        {
+            Log.d(TAG, "Alarm On");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimerPicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimerPicker.getCurrentMinute());
+            Intent newIntent = new Intent(this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this,
+                                            0, newIntent, 0);
+            alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+        }
+        else
+        {
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d(TAG, "Alarm Off");
+        }
+    }
+
+
+    //------------------------- user function ---------------------------//
+    private void initView()
+    {
+        Log.w(TAG, " initView(), ");
+        alarmTimerPicker = findViewById(R.id.alarmTimerPicker);
+        alarmToggle = findViewById(R.id.alarmToggle);
+        alarmTextView = findViewById(R.id.alarmText);
+    }
+
+    private void initControl()
+    {
+        Log.w(TAG, " initControl(), ");
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+    }
+
+    public void setAlarmText(String alarmText)
+    {
+        alarmTextView.setText(alarmText);
+    }
+
+
+}
